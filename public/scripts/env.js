abc9c72304cb5e3beda6a3341c05aa9031a9f267
OrlaGamer09 <orlandoarroyoo02@gmail.com>
2025-04-08 10:31:14 -0500

    test busmarker and new array [] v2

--- public/scripts/env.js
+++ public/scripts/env.js
@@ -1,5 +1,5 @@
 // scripts/env.js
 // Este archivo NO se sube a GitHub, solo se usa localmente
 window.ENV = {
-    API_KEY1: 'AIzaSyCpLAZyMQEMB4IL6KvOQGhyA_DWD7Wt3jE' // Reemplaza con tu clave del .env
+    API_KEY1: 'clave' // Reemplaza con tu clave del .env
 };
